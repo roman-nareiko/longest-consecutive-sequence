7fd441d6ae82ed8afe4cab4dad8c634598e80e10
Raman Nareika <Raman.Nareika@thomsonreuters.com>
2018-03-06 16:47:49 +0300

    impl

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  const set = new Set(array);
+  let length = 0;
+
+  set.forEach(element => {
+    if(!set.has(element - 1)) {
+      let start = element;
+
+      while(set.has(start))
+        start++;
+
+      length = Math.max(length, start - element);
+    }
+  });
+
+  return length;
 }
